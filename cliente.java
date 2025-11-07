672765bb3875a204038db88db18f2447fa68150b
Fábio Félix Rodrigues <fabiorod.it@gmail.com>
2025-11-07 00:36:57 -0300

    criação de classes pessoa, cliente e funcionario

---
+++ cliente.java
@@ -0,0 +1,30 @@
+public class cliente extends pessoa{
+    static int contador = 0;
+    private int id;
+    private String tipoCliente;
+    private double renda;
+
+    public cliente(String nome, String cpf, String endereco, String telefone, String tipoCliente, double renda){
+        super(nome, cpf, endereco, telefone);
+        contador ++;
+        id = contador;
+        this.tipoCliente = tipoCliente;
+        this.renda = renda;
+    }
+
+    public String getTipoCliente(){
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente){
+        this.tipoCliente = tipoCliente;
+    }
+
+    public double getRenda(){
+        return renda;
+    }
+
+    public void setRenda(double renda){
+        this.renda = renda;
+    }
+}
--- funcionario.java
+++ funcionario.java
@@ -1,13 +1,41 @@
 public class funcionario extends pessoa {
-    private String matricula;
+    private int matricula;
     private String cargo;
     private double salario;
 
 
-    public funcionario(String nome, String cpf, String endereco, String telefone, String matricula, String cargo, double salario){
+    public funcionario(String nome, String cpf, String endereco, String telefone, int matricula, String cargo, double salario){
         super(nome, cpf, endereco, telefone);
         this.matricula = matricula;
         this.cargo = cargo;
         this.salario = salario;
+    }
+
+    public int getMatricula(){
+        return matricula;
+    }
+
+    public void setMatricula(int matricula){
+        this.matricula = matricula;
+    }
+
+    public String getCargo(){
+        return cargo;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    public double calcularBonificacao(){
+        return salario * 0.10;
     }
 }
